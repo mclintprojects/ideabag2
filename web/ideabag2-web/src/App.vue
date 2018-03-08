1ae00551cced57bec0c26cc0caf513e4e46c5810
Mbah Clinton <clintonmbah44@gmail.com>
2018-03-08 20:12:09 +0000

    First draft complete. Damn I love Vue.js

---
+++ web/ideabag2-web/src/App.vue
@@ -0,0 +1,129 @@
+<template>
+  <div>
+    <div id="toolbar" class="navbar navbar-fixed-top">
+      <h4>IdeaBag 2</h4>
+    </div>
+    <img id="loadingCircle" v-if="isLoading" src="http://samherbert.net/svg-loaders/svg-loaders/oval.svg" />
+    <div id="componentHolder" class="row">
+      <div class="col-xs-3 col-lg-3">
+        <category-list :categories="categories" @categoryClicked="selectCategory($event)"></category-list>        
+      </div>
+      <div class="col-xs-3 col-lg-3">
+        <idea-list :ideas="selectedCategoryIdeas" @ideaClicked="selectIdea($event)"></idea-list>
+      </div>
+      <div class="col-xs-6 col-lg-6">
+        <idea-detail :idea="selectedIdea"></idea-detail>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import CategoryList from './components/CategoryList';
+import IdeaList from './components/IdeaList';
+import IdeaDetail from './components/IdeaDetail';
+
+let ideasURL =
+	'https://docs.google.com/document/d/17V3r4fJ2udoG5woDBW3IVqjxZdfsbZC04G1A-It_DRU/export?format=txt';
+
+export default {
+	name: 'app',
+	data() {
+		return {
+			categories: [],
+			selectedCategoryIdeas: null,
+			selectedIdea: null,
+			categorySelectionIndex: 0,
+			ideaSelectionIndex: 0,
+			isLoading: true
+		};
+	},
+	methods: {
+		selectCategory(index) {
+			this.selectedCategoryIdeas = this.categories[index].items;
+			this.selectedIdea = null;
+		},
+		selectIdea(index) {
+			this.selectedIdea = this.selectedCategoryIdeas[index];
+		}
+	},
+	components: {
+		categoryList: CategoryList,
+		ideaList: IdeaList,
+		ideaDetail: IdeaDetail
+	},
+	created() {
+		this.$http.get(ideasURL).then(
+			response => {
+				this.isLoading = false;
+				this.categories = response.body;
+				console.log(response.body);
+			},
+			error => {
+				this.isLoading = false;
+				alert('Something went wrong!');
+				console.log(error);
+			}
+		);
+	}
+};
+</script>
+
+<style>
+:root {
+	--primary: #ffa000;
+	--primaryDark: #c67100;
+	--background: #37474f;
+	--highlight: #2c393f;
+	--primaryText: rgba(255, 255, 255, 0.8);
+	--primaryTextLight: rgba(255, 255, 255, 0.54);
+}
+
+body {
+	background-color: var(--background);
+	font-family: 'Roboto', sans-serif;
+	overflow-x: hidden;
+}
+
+#loadingCircle {
+	width: 36px;
+	position: absolute;
+	left: 50%;
+	top: 50%;
+}
+
+.primaryLbl {
+	color: var(--primaryText);
+}
+
+.secondaryLbl {
+	color: var(--primaryTextLight);
+}
+
+.col-xs-3,
+.col-xs-6,
+.row {
+	margin: 0px;
+	padding: 0px;
+}
+
+.navbar {
+	background-color: var(--primary);
+	border-radius: 0px;
+}
+
+#toolbar {
+	display: flex;
+	align-items: center;
+	box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.1);
+}
+
+#toolbar h4 {
+	margin-left: 16px;
+	color: rgba(255, 255, 255, 0.8);
+}
+
+#componentHolder {
+	margin-top: 70px;
+}
+</style>
