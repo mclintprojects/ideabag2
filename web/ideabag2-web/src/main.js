bbe195c1eda62ff5c26a829f570d851a45f8b593
Mbah Clinton <clintonmbah44@gmail.com>
2018-03-09 14:54:55 +0000

    More additions to facilitate the move to a SPA

--- web/ideabag2-web/src/main.js
+++ web/ideabag2-web/src/main.js
@@ -1,10 +1,16 @@
 import Vue from 'vue';
 import App from './App.vue';
 import VueResource from 'vue-resource';
+import VueRouter from 'vue-router';
+import { routes } from './routes';
 
 Vue.use(VueResource);
+Vue.use(VueRouter);
+
+const router = new VueRouter({ routes });
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  router
 });
---
+++ web/ideabag2-web/src/routes.js
@@ -0,0 +1,9 @@
+import CategoryList from './components/CategoryList';
+import IdeaList from './components/IdeaList';
+import IdeaDetail from './components/IdeaDetail';
+
+export const routes = [
+  { path: '', component: CategoryList },
+  { path: '/ideas', component: IdeaList },
+  { path: '/ideas/:id/detail', component: IdeaDetail }
+];
