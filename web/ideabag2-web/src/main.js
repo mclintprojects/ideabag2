327e3e50a328a8da010582c43bfa042650306e7c
Mbah Clinton <clintonmbah44@gmail.com>
2018-03-16 08:32:53 +0000

    Deployed SPA. still need to fix event handler bug.

--- web/ideabag2-web/src/main.js
+++ web/ideabag2-web/src/main.js
@@ -7,7 +7,7 @@ import { routes } from './routes';
 Vue.use(VueResource);
 Vue.use(VueRouter);
 
-const router = new VueRouter({ routes });
+const router = new VueRouter({ routes, mode: 'history' });
 
 new Vue({
 	el: '#app',
