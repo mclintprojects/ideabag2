13b3ae686f896e7bbd30456bf9a015c69406d396
Mbah Clinton <clintonmbah44@gmail.com>
2018-06-17 12:02:50 +0000

    added comment date, highlight links in comments

--- web/ideabag2-web/src/eventbus.js
+++ web/ideabag2-web/src/eventbus.js
@@ -1,19 +1,5 @@
 import Vue from 'vue';
 
-const eventbus = new Vue({
-	methods: {
-		showToast(message, type = 'default', toastLength = 'short') {
-			var duration = toastLength == 'short' ? 3000 : 5000;
-
-			this.$toasted.show(message, {
-				duration,
-				position: 'bottom-center',
-				singleton: true,
-				type,
-				theme: 'bubble'
-			});
-		}
-	}
-});
+const eventbus = new Vue();
 
 export default eventbus;
