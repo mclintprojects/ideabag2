c452758d4b91a0d94c66ed89c3a84d19f2c0be09
Mbah Clinton <clintonmbah44@gmail.com>
2018-06-17 12:02:50 +0000

    added comment date, highlight links in comments

--- android/kotlin/app/src/main/java/com/alansa/ideabag2/models/Comment.kt
+++ android/kotlin/app/src/main/java/com/alansa/ideabag2/models/Comment.kt
@@ -1,5 +1,12 @@
 package com.alansa.ideabag2.models
 
+import java.text.DateFormat
+import java.util.*
+
 data class Comment(val id: String, val author: String, val comment: String, val created: Long) {
+    fun getDateString() : String{
+        return DateFormat.getDateInstance(DateFormat.SHORT).format(Date(created))
+    }
+
     fun toMap() = hashMapOf("author" to author, "comment" to comment, "created" to created)
 }
