0406917d3c1ab84d0e262b686b5311b382c48e9b
Mbah Clinton <clintonmbah44@gmail.com>
2018-06-19 20:18:10 +0000

    fixed ideabag website failing issue

--- web/ideabag2-web/src/eventbus.js
+++ web/ideabag2-web/src/eventbus.js
@@ -1,5 +1,19 @@
 import Vue from 'vue';
 
-const eventbus = new Vue();
+const eventbus = new Vue({
+  methods: {
+    showToast(message, type = 'default', toastLength = 'short') {
+      var duration = toastLength == 'short' ? 3000 : 5000;
+
+      this.$toasted.show(message, {
+        duration,
+        position: 'bottom-center',
+        singleton: true,
+        type,
+        theme: 'bubble'
+      });
+    }
+  }
+});
 
 export default eventbus;
