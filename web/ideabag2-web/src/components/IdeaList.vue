848d47d4caccbb6e43c62e8ab9fba5a6b21bb284
Jarik Marwede <accounts.jarikmarwede@zoho.eu>
2018-10-13 14:46:08 +0200

    Factored out idea list into its own component for reuse

---
+++ web/ideabag2-web/src/components/IdeaList.vue
@@ -0,0 +1,66 @@
+<template>
+  <ul id="ideaList">
+		<li v-for="(idea, index) in ideas" :key="index" @click="notifyIdeaClicked(index)" :class="{highlight: index == selectedIndex}">
+			<div class="ideaItem">
+				<p id="ideaTitle" class="primaryLbl">{{idea.title}}</p>
+				<p id="ideaDifficulty" class="badge secondaryLbl">{{idea.difficulty}}</p>
+			</div>
+		</li>
+	</ul>
+</template>
+
+<script>
+export default {
+  computed: {
+    selectedIndex() {
+			return this.$store.getters.selectedIdeaIndex;
+		}
+  },
+  props: {
+    ideas: {
+      type: Array,
+      required: true
+    }
+  },
+  methods: {
+    notifyIdeaClicked(index) {
+			this.$router.push({ name: 'ideas', params: { categoryId: this.$route.params.categoryId, ideaId: index } });
+
+			this.$store.dispatch('setSelectedIdeaIndex', index);
+		}
+  }
+};
+</script>
+
+<style scoped>
+#ideaList {
+	list-style-type: none;
+	margin: 0px;
+	padding: 0px;
+}
+
+#ideaList li {
+	padding: 8px 16px 8px 16px;
+}
+
+#ideaList li:hover {
+	background-color: var(--highlight);
+	cursor: pointer;
+}
+
+#ideaTitle {
+	font-size: var(--primaryTextSize);
+	margin: 0px 0px 4px 0px;
+	text-overflow: ellipsis;
+	overflow: hidden;
+	white-space: nowrap;
+	padding: 0px;
+}
+
+.badge {
+	background-color: var(--primary);
+	padding: var(--badgePadding);
+	color: rgba(0, 0, 0, 0.54);
+	font-size: var(--badgeTextSize);
+}
+</style>
