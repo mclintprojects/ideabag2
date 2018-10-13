e5e6ffbf454aa1696052d0c19c3bd65d3eec9ea3
Jarik Marwede <accounts.jarikmarwede@zoho.eu>
2018-10-13 14:46:08 +0200

    Factored out idea list into its own component for reuse

--- web/ideabag2-web/src/views/IdeasList.vue
+++ web/ideabag2-web/src/views/IdeasList.vue
@@ -1,18 +1,13 @@
 <template>
 	<div class="appContainer">
 		<img v-if="isLoading" id="loadingCircle" src="https://samherbert.net/svg-loaders/svg-loaders/oval.svg" />
-		<ul id="ideaList">
-			<li v-for="(idea, index) in ideas" :key="index" @click="notifyIdeaClicked(index)" :class="{highlight: index == selectedIndex}">
-				<div class="ideaItem">
-					<p id="ideaTitle" class="primaryLbl">{{idea.title}}</p>
-					<p id="ideaDifficulty" class="badge secondaryLbl">{{idea.difficulty}}</p>
-				</div>
-			</li>
-		</ul>
+		<idea-list :ideas="ideas" />
 	</div>
 </template>
 
 <script>
+import IdeaList from "../components/IdeaList";
+
 export default {
 	data() {
 		return {
@@ -22,17 +17,10 @@ export default {
 	computed: {
 		isLoading() {
 			return this.$store.getters.categories.length == 0;
-		},
-		selectedIndex() {
-			return this.$store.getters.selectedIdeaIndex;
 		}
 	},
-	methods: {
-		notifyIdeaClicked(index) {
-			this.$router.push({ name: 'ideas', params: { categoryId: this.$route.params.categoryId, ideaId: index } });
-
-			this.$store.dispatch('setSelectedIdeaIndex', index);
-		}
+	components: {
+		"idea-list": IdeaList
 	},
 	activated() {
 		if (this.$store.getters.categories) {
@@ -45,36 +33,3 @@ export default {
 	}
 };
 </script>
-
-<style scoped>
-#ideaList {
-	list-style-type: none;
-	margin: 0px;
-	padding: 0px;
-}
-
-#ideaList li {
-	padding: 8px 16px 8px 16px;
-}
-
-#ideaList li:hover {
-	background-color: var(--highlight);
-	cursor: pointer;
-}
-
-#ideaTitle {
-	font-size: var(--primaryTextSize);
-	margin: 0px 0px 4px 0px;
-	text-overflow: ellipsis;
-	overflow: hidden;
-	white-space: nowrap;
-	padding: 0px;
-}
-
-.badge {
-	background-color: var(--primary);
-	padding: var(--badgePadding);
-	color: rgba(0, 0, 0, 0.54);
-	font-size: var(--badgeTextSize);
-}
-</style>
