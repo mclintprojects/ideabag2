f30d9a8c0dcd27e2e657cce67e2780e9cc9923d5
Jarik Marwede <accounts.jarikmarwede@zoho.eu>
2018-11-23 17:58:34 +0100

    Factored out multiple database accessing methods into mixin

---
+++ web/src/mixins/UserDataDBInterface.js
@@ -0,0 +1,42 @@
+export default {
+  computed: {
+    userDataDB() {
+      return this.$store.getters.userDataDB;
+    }
+  },
+  methods: {
+    addToBookmarks(id) {
+			const objectStore = this.userDataDB
+				.transaction(['ideas'], 'readwrite')
+				.objectStore('ideas');
+			objectStore.get(id).onsuccess = event => {
+				if (event.target.result === undefined) {
+					this.addNewIdea(id, true, 'undecided');
+				} else {
+					event.target.result.bookmarked = 1;
+					objectStore.put(event.target.result)
+				}
+			};
+		},
+    removeFromBookmarks(id) {
+			const objectStore = this.userDataDB
+				.transaction(['ideas'], 'readwrite')
+				.objectStore('ideas');
+			objectStore.get(id).onsuccess = event => {
+				event.target.result.bookmarked = 0;
+				objectStore.put(event.target.result)
+			};
+		},
+    addNewIdea(ideaId, bookmarked, progress) {
+			const bookmarkedBinary = bookmarked ? 1 : 0;
+			this.userDataDB
+				.transaction(['ideas'], 'readwrite')
+				.objectStore('ideas')
+				.add({
+					id: ideaId,
+					bookmarked: bookmarkedBinary,
+					progress: progress
+				})
+    }
+  }
+}
