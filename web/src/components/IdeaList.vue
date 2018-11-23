cca08f78f61941dbfaf6a84d46f79b0bae32250b
Jarik Marwede <accounts.jarikmarwede@zoho.eu>
2018-11-23 18:00:44 +0100

    Added bookmarking menu on idea list

--- web/src/components/IdeaList.vue
+++ web/src/components/IdeaList.vue
@@ -2,8 +2,19 @@
   <ul id="ideaList">
 		<li v-for="(idea, index) in ideas" :key="index" @click="notifyIdeaClicked(idea, index)" :class="{'highlight': index === selectedIndex, 'progress-undecided': idea.progress === 'undecided', 'progress-in-progress': idea.progress === 'in-progress', 'progress-done': idea.progress === 'done'}">
 			<div class="ideaItem">
-				<p id="ideaTitle" class="primaryLbl">{{idea.title}}</p>
-				<p id="ideaDifficulty" class="badge secondaryLbl">{{idea.difficulty}}</p>
+        <div>
+  				<p id="ideaTitle" class="primaryLbl">{{idea.title}}</p>
+  				<p id="ideaDifficulty" class="badge secondaryLbl">{{idea.difficulty}}</p>
+        </div>
+        <div>
+          <popper trigger="click" :options="{placement: 'left'}" @created="openNewPopper">
+            <div class="popper idea-actions">
+              <button @click.stop="toggleBookmark(index)" v-show="idea.bookmarked">Remove bookmark</button>
+              <button @click.stop="toggleBookmark(index)" v-show="!idea.bookmarked">Bookmark</button>
+            </div>
+            <img slot="reference" src="../../public/img/baseline-more_vert-24px.svg" alt="Idea actions" @click.stop>
+          </popper>
+        </div>
 			</div>
 		</li>
 	</ul>
@@ -11,14 +22,22 @@
 
 <script>
 import Vue from 'vue';
+import Popper from 'vue-popperjs';
+import UserDataDBInterface from '../mixins/UserDataDBInterface';
 
 export default {
+  mixins: [UserDataDBInterface],
+  components: {
+    'popper': Popper
+  },
+  data() {
+    return {
+      openPopper: null
+    }
+  },
   computed: {
     selectedIndex() {
       return this.$store.getters.selectedIdeaIndex;
-    },
-    userDataDB() {
-      return this.$store.getters.userDataDB;
     }
   },
   props: {
@@ -30,17 +49,17 @@ export default {
   watch: {
     userDataDB(db) {
       if (db !== null && this.ideas.length > 0) {
-        this.loadProgressData();
+        this.loadIdeaData();
       }
     },
     ideas(ideas) {
       if (this.userDataDB !== null && ideas.length > 0) {
-        this.loadProgressData();
+        this.loadIdeaData();
       }
     }
   },
   methods: {
-    loadProgressData() {
+    loadIdeaData() {
       for (let i = 0; i < this.ideas.length; i++) {
         this.userDataDB
           .transaction(['ideas'])
@@ -51,8 +70,10 @@ export default {
           if (this.ideas.length > i) {
             if (event.target.result !== undefined) {
               Vue.set(this.ideas[i], 'progress', event.target.result.progress);
+              Vue.set(this.ideas[i], 'bookmarked', event.target.result.bookmarked ? true : false);
             } else {
               Vue.set(this.ideas[i], 'progress', 'undecided');
+              Vue.set(this.ideas[i], 'bookmarked', false);
             }
           }
         };
@@ -68,16 +89,34 @@ export default {
         params: { categoryId: categoryId, ideaId: idea.id }
       });
       this.$store.dispatch('setSelectedIdeaIndex', index);
+    },
+    toggleBookmark(index) {
+      const idea = this.ideas[index];
+      const id = `${idea.categoryId}C-${idea.id}I`;
+      if (idea.bookmarked) {
+        this.removeFromBookmarks(id);
+        idea.bookmarked = false;
+      } else {
+        this.addToBookmarks(id);
+        idea.bookmarked = true;
+      }
+      this.openPopper.doClose();
+    },
+    openNewPopper(context) {
+      if (this.openPopper && this.openPopper !== context) {
+        this.openPopper.doClose();
+      }
+      this.openPopper = context;
     }
   },
   activated() {
     if (this.userDataDB !== null && this.ideas.length > 0) {
-      this.loadProgressData();
+      this.loadIdeaData();
     }
   },
   created() {
     if (this.userDataDB !== null && this.ideas.length > 0) {
-      this.loadProgressData();
+      this.loadIdeaData();
     }
   }
 };
@@ -105,6 +144,11 @@ export default {
   white-space: nowrap;
   padding: 0px;
 }
+.ideaItem {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
 .progress-undecided {
   border-left: 8px solid var(--undecided) !important;
 }
@@ -119,5 +163,19 @@ export default {
   padding: var(--badgePadding);
   color: rgba(0, 0, 0, 0.54);
   font-size: var(--badgeTextSize);
+}
+.idea-actions {
+  border: none;
+  display: flex;
+  flex-flow: column;
+  padding: 0;
+}
+.idea-actions > button {
+  background-color: transparent;
+  border: none;
+  padding: 1rem;
+}
+.idea-actions > button:hover {
+  background-color: rgba(0, 0, 0, 0.2);
 }
 </style>
--- web/src/main.js
+++ web/src/main.js
@@ -5,10 +5,12 @@ import { routes } from './routes';
 import { store } from './store/store';
 import Toasted from 'vue-toasted';
 import vmodal from 'vue-js-modal';
+import Popper from 'vue-popperjs';
 
 Vue.use(VueRouter);
 Vue.use(Toasted);
 Vue.use(vmodal);
+Vue.use(Popper);
 
 const router = new VueRouter({ routes, mode: 'history' });
 
