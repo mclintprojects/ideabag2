419c37fc6981f2da79b12ec8c0e345927d259837
Mbah Clinton <clintonmbah44@gmail.com>
2018-12-13 18:00:33 +0000

    added initial unit tests

--- web/test/components/Navbar.test.js
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils';
-import Navbar from '../../src/components/Navbar.vue';
-
-describe('Navbar.vue', () => {
-  test('it should render content correctly', () => {
-    const wrapper = mount(Navbar);
-    expect(wrapper.find('#toolbar > h4').text()).toEqual('Ideabag 2 (BETA)');
-  });
-});
---
+++ web/tests/unit/components/Navbar.spec.js
@@ -0,0 +1,54 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import Navbar from '../../../src/components/Navbar.vue';
+import Vuex from 'vuex';
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+
+describe('Navbar.vue', () => {
+  let store;
+  beforeEach(() => {
+    store = new Vuex.Store({
+      modules: {
+        app: {
+          getters: {
+            navbarTitle: () => 'Ideabag 2 (TEST)'
+          }
+        }
+      }
+    });
+  });
+
+  test('It should render app title correctly', () => {
+    const wrapper = shallowMount(Navbar, {
+      store,
+      localVue,
+      stubs: ['router-link', 'font-awesome-icon'],
+      mocks: { $route: { path: '/' } }
+    });
+
+    expect(wrapper.find('#toolbar > h4').text()).toEqual('Ideabag 2 (TEST)');
+  });
+
+  test('It should not render back button if root route', () => {
+    const wrapper = shallowMount(Navbar, {
+      store,
+      localVue,
+      stubs: ['router-link', 'font-awesome-icon'],
+      mocks: { $route: { path: '/' } }
+    });
+
+    expect(wrapper.find('#backBtn').exists()).toEqual(false);
+  });
+
+  test('It should render back button if not root route', () => {
+    const wrapper = shallowMount(Navbar, {
+      store,
+      localVue,
+      stubs: ['router-link', 'font-awesome-icon'],
+      mocks: { $route: { path: '/categories/1' } }
+    });
+
+    expect(wrapper.find('#backBtn').exists()).toEqual(true);
+  });
+});
