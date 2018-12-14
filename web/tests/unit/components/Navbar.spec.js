a37b9e4576b0cc65cdb36e4e07e72c7d3fa67d33
Mbah Clinton <clintonmbah44@gmail.com>
2018-12-14 07:14:14 +0000

    added more tests

--- web/tests/unit/components/Navbar.spec.js
+++ web/tests/unit/components/Navbar.spec.js
@@ -1,6 +1,7 @@
 import { shallowMount, createLocalVue } from '@vue/test-utils';
 import Navbar from '../../../src/components/Navbar.vue';
 import Vuex from 'vuex';
+import StoreFactory from '../StoreFactory';
 
 const localVue = createLocalVue();
 localVue.use(Vuex);
@@ -8,15 +9,7 @@ localVue.use(Vuex);
 describe('Navbar.vue', () => {
   let store;
   beforeEach(() => {
-    store = new Vuex.Store({
-      modules: {
-        app: {
-          getters: {
-            navbarTitle: () => 'Ideabag 2 (TEST)'
-          }
-        }
-      }
-    });
+    store = StoreFactory({ navbarTitle: () => 'Ideabag 2 (TEST)' });
   });
 
   test('It should render app title correctly', () => {
@@ -50,5 +43,57 @@ describe('Navbar.vue', () => {
     });
 
     expect(wrapper.find('#backBtn').exists()).toEqual(true);
+  });
+
+  test('It should render correct links when logged in', () => {
+    let store = StoreFactory({
+      userLoggedIn: () => true,
+      userEmail: () => 'test@gmail.com'
+    });
+
+    const wrapper = shallowMount(Navbar, {
+      store,
+      localVue,
+      stubs: ['router-link', 'font-awesome-icon'],
+      mocks: { $route: { path: '/' } }
+    });
+
+    expect(wrapper.find('#links li:first-child').text()).toEqual(
+      'Welcome, test@gmail.com!'
+    );
+    expect(wrapper.find('#links li:last-child').text()).toEqual('Log out');
+  });
+
+  test('It should render correct links when logged out', () => {
+    let store = StoreFactory({
+      userLoggedIn: () => false
+    });
+
+    const wrapper = shallowMount(Navbar, {
+      store,
+      localVue,
+      stubs: ['router-link', 'font-awesome-icon'],
+      mocks: { $route: { path: '/' } }
+    });
+
+    expect(wrapper.find('#links li:first-child').text()).toEqual('Login');
+    expect(wrapper.find('#links li:last-child').text()).toEqual('Signup');
+  });
+
+  test('It should logout correctly', () => {
+    let actions = {
+      logout: jest.fn()
+    };
+    let store = StoreFactory({ userLoggedIn: () => true }, actions);
+
+    let wrapper = shallowMount(Navbar, {
+      store,
+      localVue,
+      stubs: ['router-link', 'font-awesome-icon'],
+      mocks: { $route: { path: '/' } }
+    });
+
+    wrapper.find('#links li:last-child > a').trigger('click');
+    expect(actions.logout).toHaveBeenCalled();
   });
 });
