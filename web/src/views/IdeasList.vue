6aa235ec49fd845370c8899410a75180001f260e
Jarik Marwede <accounts.jarikmarwede@zoho.eu>
2018-12-20 17:56:25 +0100

    Improved performance of loading ideas in idea list

--- web/src/views/IdeasList.vue
+++ web/src/views/IdeasList.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container-full">
     <font-awesome-icon class="loader" v-if="isLoading" icon="spinner" size="3x" spin fixed-width></font-awesome-icon>
-    <idea-list :ideas="ideas"/>
+    <idea-list v-if="!isLoading && ideasLoaded" :ideas="ideas"/>
   </div>
 </template>
 
@@ -11,23 +11,27 @@ import IdeaList from '../components/IdeaList';
 export default {
   data() {
     return {
-      ideas: []
+      ideas: [],
+      ideasLoaded: false
     };
   },
   computed: {
     isLoading() {
-      return this.$store.getters.categories.length == 0;
+      return this.$store.getters.isLoading;
     }
   },
   components: { IdeaList },
   activated() {
-    if (this.$store.getters.categories) {
-      const categoryIndex = this.$route.params.categoryId - 1;
-      const title = this.$store.getters.categories[categoryIndex].categoryLbl;
-      this.$store.dispatch('setTitle', title);
+    const categoryIndex = this.$route.params.categoryId - 1;
+    const title = this.$store.getters.categories[categoryIndex].categoryLbl;
+    this.$store.dispatch('setTitle', title);
 
-      this.ideas = this.$store.getters.categories[categoryIndex].items;
-    }
+    this.ideas = this.$store.getters.categories[categoryIndex].items;
+    this.ideasLoaded = true;
+  },
+  deactivated() {
+    this.ideas = []
+    this.ideasLoaded = false;
   }
 };
 </script>
