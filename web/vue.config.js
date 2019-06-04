19806007fdf18f59fb4e1a242d6acc8ae66d1e94
Jarik Marwede <accounts.jarikmarwede@zoho.eu>
2019-06-04 21:11:04 +0200

    Configured PWA plugin

---
+++ web/vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  pwa: {
+    name: 'Idea Bag',
+    themeColor: '#ffa000',
+    msTileColor: '#37474f',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: '#ffa000',
+  }
+};
